6dbbc758e7e1da150fca216965ae20a14619ebe1
Ricardo Fernandes <ricardoaf@lccv.ufal.br>
2020-03-16 13:58:22 -0300

    add active/inactive areas history

--- src/TrussView.m
+++ src/TrussView.m
@@ -1,7 +1,9 @@
 function TrussView(xH,fH,Time,fem,Filter)
 %% --------------------------------------------- Apply visualization filter
 x = xH(:,end);
-xEff = x; xEff(x<Filter*max(x)) = 0;
+inactive = find(x<Filter*max(x));
+active = setdiff((1:length(x))',inactive);
+xEff = x; xEff(inactive) = 0;
 %% -------------------------------------------------------- Summary results
 fprintf('Number of bars: %d of %d\n', nnz(xEff), length(x));
 fprintf('Objective function value: %g\n',fH(end));
@@ -12,16 +14,30 @@ figure('Color',[1 1 1]); box on; grid on; hold on;
 plot(fH,'r-o','MarkerEdgeColor','b','MarkerFaceColor','w','MarkerSize',4);
 xlabel('Iteration'); ylabel('Objective function value');
 title('Objective function history'); drawnow;
+%% --------------------------------------------------- Active areas history
+figure('Color',[1 1 1]); box on; grid on; hold on;
+for i = active'
+    plot(xH(i,:),'k-');
+end
+xlabel('Iteration'); ylabel('area value');
+title('Active areas history'); drawnow;
+%% ------------------------------------------------- Inactive areas history
+figure('Color',[1 1 1]); box on; grid on; hold on;
+for i = inactive'
+    plot(xH(i,:),'k-');
+end
+xlabel('Iteration'); ylabel('area value');
+title('Inactive areas history'); drawnow;
 %% ------------------------------------------------- Member areas histogram
 figure('Color',[1 1 1]); box on; grid on; hold on;
-bar(sort(xEff(xEff>0))./max(x),'b'); xlim([.5 nnz(xEff)+.5]);
+bar(sort(xEff(xEff>0))./max(x),'k'); xlim([.5 nnz(xEff)+.5]);
 xlabel('Sorted active member'); ylabel('Normalized area');
 title('Active member areas'); drawnow;
 %% --------------------------------------------------------- Final topology
 figure('Color',[1 1 1]); box off; hold on; grid off; axis equal;
 for e = find(xEff>0)'
     X = fem.Node(fem.Element(e,:),1); Y = fem.Node(fem.Element(e,:),2);
-    clr = 'b'; if fem.stress(e)<0, clr='r'; end 
+    clr = 'b'; if fem.stress(e)<0, clr='r'; end
     plot(X,Y,[clr '-'],'LineWidth',xEff(e)/max(x)*5);
 end
 for e = find(xEff>0)'
